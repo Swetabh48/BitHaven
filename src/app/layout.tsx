eaee0dd7086a92c607b92030e60dba5ab2a4cb71
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-08-01 13:13:30 +0530

    customization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const dmSans=DM_Sans({
+  subsets:["latin"],
 });
 
 export const metadata: Metadata = {
@@ -20,12 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode; 
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSans.className}antialiased`}
       >
         {children}
       </body>
