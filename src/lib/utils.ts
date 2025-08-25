817aa656e35c3950e6cfbc83a1e7b72f4f51a74b
Swetabh Salampuria <swetabhsalampuria48@gmail.com>
2025-08-25 23:43:53 +0530

    sub-domain rewrites

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,20 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function generateTenantURL(tenantSlug: string) {
-  return `/tenants/${tenantSlug}`;
+export function generateTenantURL(tenantSlug: string ) {
+  // In development mode,use normal routing
+  if(process.env.NODE_ENV==="development"){
+    return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
+  }
+  const protocol="https";
+  const domain=process.env.NEXT_PUBLIC_ROOT_DOMAIN!
+
+  // if(process.env.NODE_ENV==="development"){
+  //   protocol="http";
+  // }
+
+  //In production, use subdomain routing
+  return `${protocol}://${tenantSlug}.${domain}`;
 }
 
 export function formatCurrency(value:number|string){
--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -6,6 +6,7 @@ import z from "zod";
 import { CheckoutMetadata, ProductMetadata } from "../types";
 import { stripe } from "@/lib/stripe";
 import { PLATFORM_FEE_PERCENT } from "@/constants";
+import { generateTenantURL } from "@/lib/utils";
 
 
 
@@ -144,10 +145,19 @@ export const checkoutRouter=createTRPCRouter({
             totalAmount*(PLATFORM_FEE_PERCENT/100)
         )
 
+        const domain=generateTenantURL(input.tenantSlug)
+
+        // if(process.env.NODE_ENV==="development"){
+        //     domain=`${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}`
+        // }
+        // else{
+        //     domain=`${input.tenantSlug}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
+        // }
+
         const checkout=await stripe.checkout.sessions.create({
             customer_email:ctx.session.user.email,
-            success_url:`${process.env.NEXT_PUBLIC_APP_URL!}/tenants/${input.tenantSlug}/checkout?success=true`,
-            cancel_url:`${process.env.NEXT_PUBLIC_APP_URL!}/tenants/${input.tenantSlug}/checkout?cancel=true`,
+            success_url:`${domain}/checkout?success=true`,
+            cancel_url:`${domain}/checkout?cancel=true`,
             mode:"payment",
             line_items:lineItems,
             invoice_creation:{
